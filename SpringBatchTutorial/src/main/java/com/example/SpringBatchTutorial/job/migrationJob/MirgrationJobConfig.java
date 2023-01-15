422c1190314bd150c62865b7932bba770bf1fe6f
임한솔 <hansole10@gmail.com>
2023-01-15 21:12:24 +0900

    데이터 마이그레이션 코드작성 with spring batch

--- SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/migrationJob/MirgrationJobConfig.java
+++ SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/migrationJob/MirgrationJobConfig.java
@@ -1,19 +1,36 @@
 package com.example.SpringBatchTutorial.job.migrationJob;
 
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+
+import com.example.SpringBatchTutorial.job.migrationJob.core.domain.accounts.Accounts;
+import com.example.SpringBatchTutorial.job.migrationJob.core.domain.accounts.AccountsRepository;
+import com.example.SpringBatchTutorial.job.migrationJob.core.domain.orders.Orders;
+import com.example.SpringBatchTutorial.job.migrationJob.core.domain.orders.OrdersRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,6 +44,12 @@ public class MirgrationJobConfig {
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
 	
+	@Autowired
+	private OrdersRepository ordersRepository;
+	
+	@Autowired
+	private AccountsRepository accountsRepository;
+	
 	@Bean
 	public Job migrationJob(Step migrationStep) {
 		return jobBuilderFactory.get("migrationJob")
@@ -38,22 +61,47 @@ public class MirgrationJobConfig {
 	
 	@JobScope
 	@Bean
-	public Step migrationStep(Tasklet migrationTasklet) {
+	public Step migrationStep(ItemReader ordersReader, ItemProcessor ordersProcessor, ItemWriter ordersWriter) {
 		return stepBuilderFactory.get("migrationStep")
-				.tasklet(migrationTasklet)
+				.<Orders, Accounts>chunk(5)
+				.reader(ordersReader)
+				.processor(ordersProcessor)
+				.writer(ordersWriter)
 				.build();
+	}			
+	
+	
+	@StepScope
+	@Bean
+	public RepositoryItemReader<Orders> ordersReader() {
+		return new RepositoryItemReaderBuilder<Orders>()
+		.name("ordersReader")
+		.repository(ordersRepository)
+		.methodName("findAll")
+		.pageSize(5)
+		.arguments(Arrays.asList())
+		.sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+		.build();
 	}
 	
 	@StepScope
 	@Bean
-	public Tasklet jobListenerTasklet() {
-		return new Tasklet() {
-			
+	public ItemProcessor<Orders, Accounts> orderProcessor() {
+		return new ItemProcessor<Orders, Accounts>() {
 			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				throw new Exception("Failed!!");
+			public Accounts process(Orders item) throws Exception {
+				return new Accounts(item);
 			}
 		};
+	}
+	
+	@StepScope
+	@Bean
+	public RepositoryItemWriter<Accounts> orderWriter() {
+		return new RepositoryItemWriterBuilder<Accounts>()
+				.repository(accountsRepository)
+				.methodName("save")
+				.build();
 	}
 
 }
--- SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/migrationJob/core/domain/accounts/Accounts.java
+++ SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/migrationJob/core/domain/accounts/Accounts.java
@@ -2,10 +2,22 @@ package com.example.SpringBatchTutorial.job.migrationJob.core.domain.accounts;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.example.SpringBatchTutorial.job.migrationJob.core.domain.orders.Orders;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Entity
+@ToString
+@NoArgsConstructor
 public class Accounts {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +26,12 @@ public class Accounts {
 	private Integer price;
 	private Date orderDate;
 	private Date accountDate;
+	
+	public Accounts(Orders orders) {
+		this.id = orders.getId();
+		this.orderItem = orders.getOrderItem();
+		this.price = orders.getPrice();
+		this.orderDate = orders.getOrderDate();
+		this.accountDate = new Date();
+	}
 }
